03921a6b1c294af7821c4a76d77ca9e787975769
Rob Nichols <git@nicholshayes.co.uk>
2017-04-04 09:25:17 +0100

    Remove tabs from tests

--- test/ms_code_test/delivery_calculator_test.rb
+++ test/ms_code_test/delivery_calculator_test.rb
@@ -1,13 +1,13 @@
 require "test_helper"
 
 module MsCodeTest
-	class DeliveryCalculatorTest < Minitest::Test
+  class DeliveryCalculatorTest < Minitest::Test
 
-		def test_cost_for
+    def test_cost_for
       assert_equal thresholds[0], delivery_calculator.cost_for(1)
       assert_equal thresholds[50], delivery_calculator.cost_for(51)
       assert_equal thresholds[90], delivery_calculator.cost_for(91)
-		end
+    end
 
     def delivery_calculator
       @delivery_calculator ||= DeliveryCalculator.new( thresholds: thresholds )
@@ -15,9 +15,9 @@ module MsCodeTest
 
     def thresholds
       {
-      	0 => 4.95,
-      	50 => 2.95,
-      	90 => 0
+        0 => 4.95,
+        50 => 2.95,
+        90 => 0
       }
     end
 
--- test/ms_code_test/product_test.rb
+++ test/ms_code_test/product_test.rb
@@ -1,19 +1,19 @@
 require "test_helper"
 
 module MsCodeTest
-	class ProductTest < Minitest::Test
+  class ProductTest < Minitest::Test
 
-	  def test_name
-	    assert_equal product_name, product.name
-	  end
+    def test_name
+      assert_equal product_name, product.name
+    end
 
-	  def test_code
-	    assert_equal code, product.code
-	  end
+    def test_code
+      assert_equal code, product.code
+    end
 
-	  def test_price
-	    assert_equal price, product.price
-	  end
+    def test_price
+      assert_equal price, product.price
+    end
 
 	  def product
       @product = Product.new(
@@ -21,19 +21,19 @@ module MsCodeTest
         code: code,
         price: price
       )
-	  end
+    end
 
-	  def product_name
-	    'Foo'
-	  end
+    def product_name
+      'Foo'
+    end
 
-	  def code
-	    'F01'
-	  end
+    def code
+      'F01'
+    end
 
-	  def price
-	    1.10
-	  end
+    def price
+      1.10
+    end
 
-	end
+  end
 end
